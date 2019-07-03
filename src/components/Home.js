9e62f86c250a84b78b6c139636f89f673287c164
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-07-03 10:03:39 +0200

    Adding action creators and API call

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Card from './Card';
 import { connect } from 'react-redux';
+import { fetchPosts, deletePost } from '../actions';
 
 class Home extends React.Component {
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
-        console.log('HOME => ', this.props)
-        const { posts } = this.props.posts;
+        const { posts } = this.props;
         const postList = posts.length ? posts.map(({id, body, title}) => {
             return <Card title={title} body={body} id={id} key={id}/>
         }) : (<div className="center">No posts yet</div>)
@@ -24,4 +28,4 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps, { fetchPosts })(Home);
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PostDetail from './PostDetail';
+import { fetchPosts, deletePost } from '../actions';
 
 class Post extends React.Component {
-    render() {
-        console.log('POST => ', this.props);
-        const post = this.props.post ? (<PostDetail post={this.props.post} deletePost={this.props.deletePost} history={this.props.history}/>) : (<div className="center">Loading post...</div>)
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
 
+    render() {
+        const post = this.props.post ? (
+                                    <PostDetail
+                                        post={this.props.post}
+                                        deletePost={this.props.deletePost}
+                                        history={this.props.history}
+                                    />
+                                    ) : (<div className="center">Loading post...</div>)
+        
         return (
             <div className="container">
                 {post}
@@ -18,14 +28,8 @@ class Post extends React.Component {
 const mapStateToProps = (state, ownProps) => {
     let id = parseInt(ownProps.match.params.post_id);
     return {
-        post: state.posts.posts.find(post => post.id === id)
+        post: state.posts.find(post => post.id === id)
     }
 }
 
-const mapDispatchToProps = (dispatch) => {
-    return {
-        deletePost: (id) => { dispatch({ type: 'DELETE_POST', id: id }) }
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Post);
+export default connect(mapStateToProps, { fetchPosts, deletePost })(Post);
