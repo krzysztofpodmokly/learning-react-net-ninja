adca07c389478b5a86e4b8a9dc5d58029e124b30
Krzysztof Podmokly <krzysztof.podmokly@gmail.com>
2019-07-05 11:01:02 +0200

    Adding notifications database

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// Create and Deploy Your First Cloud Functions
-// https://firebase.google.com/docs/functions/write-firebase-functions
+
+// 2. User joining notification
 
 exports.helloWorld = functions.region('europe-west1').https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+    response.send("Hello from Firebase!");
 });
+
+const createNotification = (notification => {
+    return admin.firestore().collection('notifications')
+        .add(notification)
+        .then(doc => console.log('notification added => ', doc))
+});
+
+// 1. Create notification that new project was created in database
+exports.projectCreated = functions.region('europe-west1').firestore
+    .document('projects/{projectId}')
+    .onCreate(doc => {
+        const project = doc.data();
+        const notification = {
+            content: 'Added a new project',
+            user: `${project.authorFirstName} ${project.authorLastName}`,
+            time: admin.firestore.FieldValue.serverTimestamp()
+        }
+
+        return createNotification(notification)
+})
